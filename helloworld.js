772e7205efdcde6fbbffb781fe4ce203af096852
artseven <yabkevich.artsiom.09@gmail.com>
2017-06-08 21:28:23 -0400

    Adding abstract classes

--- helloworld.js
+++ helloworld.js
@@ -1,18 +1,49 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var House = (function () {
-    function House(addr) {
+    function House(numWin, numFl, houseCol, addr, protect) {
         this.address = addr;
+        this.numWindows = numWin;
+        this.numFloors = numFl;
+        this.houseColor = houseCol;
+        this.protection = protect;
     }
     House.prototype.giveAddress = function () {
         console.log("Our house address is: " + this.address);
     };
     House.prototype.breakWindow = function () {
-        this.numWindows = --(this.numWindows);
-        console.log("Number if windows is now: " + this.numWindows);
+        if (this.protection) {
+            console.log("I HAVE PROTECTION! GET OFF MY LAWN!");
+        }
+        if (this.numWindows > 0 && !this.protection) {
+            var newWin = this.numWindows--;
+            console.log("My house now has " + newWin + "windows");
+            console.log("Number if windows is now: " + this.numWindows + "windows");
+        }
+        else {
+            console.log("All the windows are broken");
+        }
+    };
+    House.prototype.changeHouseColor = function (newHouseColor) {
+        this.houseColor = newHouseColor;
+        return this.houseColor;
     };
     return House;
 }());
-var ivanHouse = new House();
+var ivanHouse = new House(4, 1, "black", "5555 Merry Way, Candyland", true);
+var wadsonHouse = new House(7, 3, "white", "1st Penn Ave", false);
 ivanHouse.giveAddress();
+ivanHouse.breakWindow();
+var newColor = ivanHouse.changeHouseColor("blue");
+console.log("Ivan's new house color is: " + newColor);
 var Greeter = (function () {
     function Greeter(message) {
         this.message = message;
@@ -25,3 +56,28 @@ var Greeter = (function () {
 var greeter = new Greeter("Hello, world!");
 console.log(greeter.message);
 document.body.innerHTML = greeter.sayHi();
+var Animal = (function () {
+    function Animal() {
+    }
+    Animal.prototype.move = function () {
+        console.log("roamin the earth...");
+    };
+    return Animal;
+}());
+var Canine = (function (_super) {
+    __extends(Canine, _super);
+    function Canine() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Canine;
+}(Animal));
+var Dog = (function (_super) {
+    __extends(Dog, _super);
+    function Dog() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Dog.prototype.makeSound = function () {
+        console.log("Woof");
+    };
+    return Dog;
+}(Canine));
--- helloworld.ts
+++ helloworld.ts
@@ -2,31 +2,61 @@
 class House {
   // constructor CREATES your objects
   // addr is a constructor parameter
-  constructor(addr: string){
+  constructor(numWin: number, numFl: number, houseCol: string, addr: string, protect: boolean){
     this.address = addr;
+    this.numWindows = numWin;
+    this.numFloors = numFl;
+    this.houseColor = houseCol;
+    this.protection = protect;
   }
+
+
   // instance variables - what an object is
   private numWindows: number;
   private numFloors: number;
   private houseColor: string;
   private address: string;
+  private protection: boolean;
 
   // what an object does
   // void - doesn't return anything
-  public giveAddress(): void{
+  public giveAddress() : void{
     // do a bunch of work
     console.log("Our house address is: " + this.address);
   }
 
-  public breakWindow(): number{
-    this. numWindows = --(this.numWindows);
-    console.log("Number if windows is now: " + this.numWindows);
+  public breakWindow(): void{
+
+    if(this.protection){
+      console.log("I HAVE PROTECTION! GET OFF MY LAWN!");
+    }
+
+
+
+    if(this.numWindows > 0 && !this.protection ){
+    let newWin: number = this.numWindows--;
+    console.log("My house now has " + newWin + "windows");
+    console.log("Number if windows is now: " + this.numWindows + "windows");
+    }
+    else {
+      console.log("All the windows are broken");
+    }
+  }
+  public changeHouseColor(newHouseColor: string) : string {
+
+    this.houseColor = newHouseColor;
+    return this.houseColor;
   }
 }
+// ivanHouse is an object
+let ivanHouse = new House(4, 1, "black","5555 Merry Way, Candyland", true);
 
-let ivanHouse = new House();
+let wadsonHouse = new House(7, 3, "white", "1st Penn Ave", false);
 
 ivanHouse.giveAddress();
+ivanHouse.breakWindow();
+let newColor = ivanHouse.changeHouseColor("blue");
+console.log("Ivan's new house color is: " + newColor);
 
 
 
@@ -42,3 +72,21 @@ let greeter = new Greeter ("Hello, world!");
 console.log(greeter.message);
 
 document.body.innerHTML = greeter.sayHi();
+
+
+abstract class Animal {
+  abstract makeSound(): void;
+  move(): void {
+    console.log("roamin the earth...");
+  }
+}
+
+abstract class Canine extends Animal {
+  // doing a bunch of canine stuff
+}
+
+class Dog extends Canine{
+  public makeSound(): void{
+    console.log("Woof");
+  }
+}
